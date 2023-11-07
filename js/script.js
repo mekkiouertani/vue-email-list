90b238df351e4246ea8b7f218791b22e5cd07c11
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-11-07 18:27:20 +0100

    added conditions !NaN, general improvements

--- js/script.js
+++ js/script.js
@@ -5,31 +5,39 @@ const { createApp } = Vue
                 mailList: [],
                 mailToGenerate: 10,
                 nValue: "",
+                alert: false,
                 
             }
         },
         methods: {
             valueMGenerate(){
+                if(!parseInt(this.nValue)){
+                    this.nValue = "";
+                    this.alert = true
+                } else{
+                    this.alert = false
+                }
                 this.mailToGenerate = this.nValue
                 this.mailList = [];
-                this.mailGenerator();
+                this.mailGenerator;
+                this.nValue = "";
             },
             
             
         },
         mounted(){
-            
+            this.mailGenerator;
         },
         computed: {
             mailGenerator(){
-            for(let i = 0; i < this.mailToGenerate; i++){
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((mail)=>{
-                    this.$nextTick(() => { 
-                        this.mailList.push(mail.data.response)
-                    });
-                    
-                })
-            }
+                for(let i = 0; i < this.mailToGenerate; i++){
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((mail)=>{
+                        this.$nextTick(() => { 
+                            this.mailList.push(mail.data.response)
+                        });
+                        
+                    })
+                }
             }   
         }
     }).mount('#app')
